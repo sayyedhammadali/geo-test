e06477ee3c5cb61b888e5635f344733adf199e8d
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-12-23 00:44:15 +0100

    Updates

---
+++ precache-manifest.04333a8c0b2a112ee9664b38830591e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e23706888efc25d4ad933ff195eddf00",
+    "url": "/geo-test/static/media/logo.e2370688.svg"
+  },
+  {
+    "revision": "2c00cbe7126799080ee0edc2d9a76c01",
+    "url": "/geo-test/static/media/current_location.2c00cbe7.GIF"
+  },
+  {
+    "revision": "75fcdf288f3857f2f2f8",
+    "url": "/geo-test/static/js/runtime~main.75fcdf28.js"
+  },
+  {
+    "revision": "2752f0aff852569a5bd1",
+    "url": "/geo-test/static/js/main.2752f0af.chunk.js"
+  },
+  {
+    "revision": "55cec80a12ccbbce5a6d",
+    "url": "/geo-test/static/js/1.55cec80a.chunk.js"
+  },
+  {
+    "revision": "2752f0aff852569a5bd1",
+    "url": "/geo-test/static/css/main.c17b1609.chunk.css"
+  },
+  {
+    "revision": "4f5e85f295be702e62b474b9e06053dc",
+    "url": "/geo-test/index.html"
+  }
+];
